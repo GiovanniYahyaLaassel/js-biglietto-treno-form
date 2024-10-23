a6e3a80b592013d8d6c1ec3acc61d58cb67beb1a
Laassel <lassel.giovanni@gmail.com>
2024-10-23 23:44:10 +0200

    Add variables

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
-console.log('hello world');
+// console.log('hello world');
 
-// Creo le variabili per chiedere all'utente età e i kilometri
-const kilometers = parseFloat(prompt('Inserisci i kilometri che vorresti percorrere:'));
-const age = parseInt(prompt('Inserisci la tua età:'));
+// // Creo le variabili per chiedere all'utente età e i kilometri
+// const kilometers = parseFloat(prompt('Inserisci i kilometri che vorresti percorrere:'));
+// const age = parseInt(prompt('Inserisci la tua età:'));
 
-// Calcolo il prezzo base del biglietto
-let ticketPrice = kilometers * 0.21;
+// // Calcolo il prezzo base del biglietto
+// let ticketPrice = kilometers * 0.21;
 
-// Creo una variabile per gli sconti
-let discount = 0;
+// // Creo una variabile per gli sconti
+// let discount = 0;
 
-// Applico un istruzione per per fare lo sconto 
-if (age < 18) {
-    discount = (ticketPrice * 20) / 100;
-    ticketPrice = ticketPrice -discount; 
+// // Applico un istruzione per per fare lo sconto 
+// if (age < 18) {
+//     discount = (ticketPrice * 20) / 100;
+//     ticketPrice = ticketPrice -discount; 
 
-} else if(age > 65) {
-    discount = (ticketPrice * 40) / 100;
-    ticketPrice = ticketPrice -discount; 
+// } else if(age > 65) {
+//     discount = (ticketPrice * 40) / 100;
+//     ticketPrice = ticketPrice -discount; 
 
-}
+// }
 
-console.log(`Il prezzo finale del biglietto è: ${ticketPrice.toFixed(2)} €`);
+// console.log(`Il prezzo finale del biglietto è: ${ticketPrice.toFixed(2)} €`);
+
+
+// Cerco di evitare il refresh della pagina
+document.getElementById('generateTicket').addEventListener('click', function (event) {
+    event.preventDefault();
+    // Creao le variabili per prendere i dati dell'utente dal dom
+
+    const nameSurname = document.getElementById('namaSurname').value;
+    const kilometers = document.getElementById('km').value;
+    const ageCategory = document.getElementById('age').value;
+
+
+
+
+})
+
